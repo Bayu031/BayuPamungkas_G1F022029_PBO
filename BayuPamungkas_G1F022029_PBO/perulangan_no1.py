3fb120e5920e0fb34829c6d7816e463846908eb6
Bayu031 <150577523+Bayu031@users.noreply.github.com>
2023-12-08 07:11:18 -0800

    Delete BayuPamungkas_G1F022029_PBO directory

--- BayuPamungkas_G1F022029_PBO/perulangan_no1.py
+++
@@ -1,7 +0,0 @@
-b = 1
-while b <= 100:
-    if b % 10 == 0:
-        print("Bayu \nBayu \nBayu")
-    else:
-        print(b)
-    b += 1
--- BayuPamungkas_G1F022029_PBO/perulangan_no3.py
+++
@@ -1,4 +0,0 @@
-array = [10, 20, 30, 40, 50]
-
-for value in array:
-    print(value)
